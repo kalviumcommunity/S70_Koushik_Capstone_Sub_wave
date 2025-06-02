efa0f27ed24aa3766d242843ea8dcef37c5414dd
Your Name <your-email@example.com>
2025-06-02 09:36:30 +0530

    Initialized a react/frontend application

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+  email:
+   { type: String, unique: true },
   password: String,
   isAdmin: { type: Boolean, default: false } 
 });
